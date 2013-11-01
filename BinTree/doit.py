85c342a9e4037f6b57ba828c91ad2661284b9b0e
Joshua Steffensky <s9jostef@stud.uni-saarland.de>
2013-11-01 16:11:29 +0100

    1.11.

--- BinTree/doit.py
+++ BinTree/doit.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python 
 import sys
 
-print "\\section*{Hello World} \n"
-str = sys.argv[1]
-print "$"+ str + "$ \\\\"
-list = eval(str)
-a = 0
-for i in list:
-	a = a + i
-print a
+#str = sys.argv[1]
+#list = eval(str)
+print "\\tikzset{treenode/.style = {align=center, inner sep=0pt, text centered, font=\\sffamily},arn_n/.style = {treenode, circle, white, font=\\sffamily\\bfseries, draw=black,fill=black, text width=1.5em},% arbre rouge noir, noeud noir arn_r/.style = {treenode, circle, red, draw=red, text width=1.5em, very thick},% arbre rouge noir, noeud rouge arn_x/.style = {treenode, rectangle, draw=black, minimum width=0.5em, minimum height=0.5em}% arbre rouge noir, nil}"
+
+
+#print "\\section*{Hello World} \n"
+#print "$"+ str + "$ \\\\"
+#a = 0
+#for i in list:
+#	a = a + i
+#print a
