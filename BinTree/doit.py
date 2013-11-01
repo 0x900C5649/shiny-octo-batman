ddcc926bb8bc591747827d86ed1803871dc82495
Joshua Steffensky <s9jostef@stud.uni-saarland.de>
2013-11-01 18:29:59 +0100

    e

--- BinTree/doit.py
+++ BinTree/doit.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python 
 import sys
 
-#str = sys.argv[1]
-#list = eval(str)
+str = sys.argv[1]
+list = eval(str)
 print "\\tikzset{treenode/.style = {align=center, inner sep=0pt, text centered, font=\\sffamily},arn_n/.style = {treenode, circle, white, font=\\sffamily\\bfseries, draw=black,fill=black, text width=1.5em},% arbre rouge noir, noeud noir arn_r/.style = {treenode, circle, red, draw=red, text width=1.5em, very thick},% arbre rouge noir, noeud rouge arn_x/.style = {treenode, rectangle, draw=black, minimum width=0.5em, minimum height=0.5em}% arbre rouge noir, nil}"
 
 
